58d21a115a8e7cccd27b2b71aff6b70b8c29cf8a
joseph mwanzia <makamwanzia@gmail.com>
2022-11-29 14:31:53 +0300

    control flow labs

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
   // Write your code here!
-}
+  function scuberGreetingForFeet(feet){
+    if (feet<=400) {
+      return "This one is on me!"
+    }
 
-function ternaryCheckCity(){
-  // Write your code here!
+  else if (feet > 2000 && feet < 2500) {
+      return "I will gladly take your thirty bucks."
 }
+    else {
+      return "No can do."
+    }
+}
+scuberGreetingForFeet(); 
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(NYC, Pittsburgh){
   // Write your code here!
+  let city = NYC
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+
+}
+ternaryCheckCity()
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+switch (tip) {
+  case 'generous':
+return "Thank you so much.";
+    break;
+
+    case 'not as generous':
+  return "Thank you.";
+      break;
+
+  default:
+  return "Bye."
+}  
+
 }
